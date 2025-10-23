7a52f3f375a21090b4b1a692c19f780c032891bb
liviamedina-dev <livia.medina@escola.pr.gov.br>
2025-10-23 10:27:57 -0300

    .

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
+const avanca = document.querySelectorAll('.btn-proximo')
+ console.log(avanca)
 
+ avanca.forEach(button => {
+     button;addEventListener('click', function)
+        const atual = document.querySelector('.ativo')
+        const proximoPasso = 'passo-' +  this.getAttibute('da')
+        atual.classList. remove('ativo')
+        document.getElementById (proximoPasso). classList.add
+
+    })
